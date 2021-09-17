7a52da9dfdad5667f1558a8cb0a40be0f15e31f7
Jacob Johnson <johnsondljl7@yahoo.com>
2021-09-16 23:00:21 -0500

    final/

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Main from "./views/Main";
-
+import io from "socket.io-client";
 import Update from "./views/Update";
 import Create from "./views/Create";
 import { BrowserRouter, Route } from "react-router-dom";
-
 // state in here and pass it down
 function App(props) {
+    const [socket] = useState(() => io(":8000"));
     const [name, setName] = useState("");
     const [loaded, setLoaded] = useState(false);
+
+    useEffect(() => {
+        // we need to set up all of our event listeners
+        // in the useEffect callback function
+        console.log("Is this running?");
+        socket.on("Welcome", (data) => console.log(data));
+
+        return () => socket.disconnect(true);
+    }, []);
 
     return (
         <BrowserRouter>
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const cors = require("cors");
+const { Socket } = require("socket.io");
 const app = express();
 require("./server/config/mongoose.config");
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 require("./server/routes/Author.routes")(app);
-app.listen(8000, () => {
+const server = app.listen(8000, () => {
     console.log("Listening at Port 8000");
+});
+const io = require("socket.io")(server, { cors: true });
+io.on("connection", (socket) => {
+    console.log(socket.id);
+    socket.on("event_from_client", (data) => {
+        socket.broadcast.emit("send_data_to_all_other_clients");
+    });
 });
